1e4292376005fbe6911e40b4fd9cc73928abc947
alanl101 <l.alan60@yahoo.com>
2023-04-11 23:23:08 -0500

    Improvements

--- lab25.py
+++ lab25.py
@@ -1,15 +1,15 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, BatchNormalization, Reshape,UpSampling2D, Conv2D, Activation, Dropout, Flatten, Input
-from tensorflow.keras.activations import tanh
-from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from tensorflow.keras.layers import BatchNormalization, Activation, LeakyReLU, UpSampling2D, Conv2D
+from tensorflow.keras.models import Sequential, Model
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# Generator will create my image
+np.random.seed(3)
+tf.random.set_seed(3)
 
+#  generator
 generator = Sequential()
 generator.add(Dense(128*7*7, input_dim=100, activation=LeakyReLU(0.2)))
 generator.add(BatchNormalization())
@@ -21,12 +21,12 @@ generator.add(Activation(LeakyReLU(0.2)))
 generator.add(UpSampling2D())
 generator.add(Conv2D(1, kernel_size=5, padding='same', activation='tanh'))
 
-# Discriminator
+#  discriminator
 discriminator = Sequential()
-discriminator.add(Conv2D(64, kernel_size=5, strides=2, input_shape=(28,28,1), padding='same'))
+discriminator.add(Conv2D(64, kernel_size=5, strides=2, input_shape=(28,28,1), padding="same"))
 discriminator.add(Activation(LeakyReLU(0.2)))
 discriminator.add(Dropout(0.3))
-discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding="same"))
 discriminator.add(Activation(LeakyReLU(0.2)))
 discriminator.add(Dropout(0.3))
 discriminator.add(Flatten())
@@ -34,16 +34,21 @@ discriminator.add(Dense(1, activation='sigmoid'))
 discriminator.compile(loss='binary_crossentropy', optimizer='adam')
 discriminator.trainable = False
 
-# GAN model
-
+#  GAN model
 generator_input = Input(shape=(100,))
 discriminator_output = discriminator(generator(generator_input))
 gan = Model(generator_input, discriminator_output)
 gan.compile(loss='binary_crossentropy', optimizer='adam')
 gan.summary()
 
+#import os
+#if not os.path.exists("./gan_images"):
+#    os.makedirs("./gan_images")
+
+#  train
 def gan_train(epoch, batch_size, saving_interval):
     
+
     # MNIST data
     (x_train, _), (_, _) = mnist.load_data()  # only train data
     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
@@ -53,45 +58,43 @@ def gan_train(epoch, batch_size, saving_interval):
     true = np.ones((batch_size, 1))
     fake = np.zeros((batch_size, 1))
     
+    output_dir = 'output'
+ 
+
     for i in range(epoch):
-        # train discriminator with real images
-        idx = np.random.randint(0, x_train.shape[0], batch_size)
-        images = x_train[idx]
-        discriminator_loss_real = discriminator.train_on_batch(images, true)
+      # train discriminator with real images
+      idx = np.random.randint(0, x_train.shape[0], batch_size)
+      images = x_train[idx]
+      discriminator_loss_real = discriminator.train_on_batch(images, true)
 
-        # train discriminator with fake images
-        noise = np.random.normal(0, 1, (batch_size, 100))
-        gen_images = generator.predict(noise, verbose=0)
-        discriminator_loss_fake = discriminator.train_on_batch(gen_images, fake)
+      # train discriminator with fake images
+      noise = np.random.normal(0, 1, (batch_size, 100))
+      gen_images = generator.predict(noise, verbose=0)
+      discriminator_loss_fake = discriminator.train_on_batch(gen_images, fake)
 
-        # discriminaor loss
-        discriminator_loss = 0.5 * np.add(discriminator_loss_real, discriminator_loss_fake)
+      # discriminaor loss
+      discriminator_loss = 0.5 * np.add(discriminator_loss_real, discriminator_loss_fake)
 
-        # train gan with fake images - gan loss
-        generator_loss = gan.train_on_batch(noise, fake) # switch true and fake labels
+      # train gan with fake images - gan loss
+      generator_loss = gan.train_on_batch(noise, true) # note that output is true
 
-        if i % 1000 == 0:
-            print('epoch:%d' % i, ' d_loss:%.4f' % discriminator_loss, ' g_loss:%.4f' % generator_loss)
-
-        #  Saving images
-        if i % saving_interval == 0:
-            # r, c = 5, 5
-            noise = np.random.normal(0, 1, (25, 100))
-            gen_images = generator.predict(noise)
+      if i % 1000 == 0:
+          print('epoch:%d' % i, ' d_loss:%.4f' % discriminator_loss, ' g_loss:%.4f' % generator_loss)
+            #  Saving images
+      if i % saving_interval == 0:
+          # r, c = 5, 5
+          noise = np.random.normal(0, 1, (25, 100))
+          gen_images = generator.predict(noise)
 
             # Rescale images 0 - 1
-            gen_images = 0.5 * gen_images + 0.5
+          gen_images = 0.5 * gen_images + 0.5
 
-            fig, axs = plt.subplots(5, 5)
-            count = 0
-            for j in range(5):
-                for k in range(5):
-                    axs[j, k].imshow(gen_images[count, :, :, 0], cmap='gray')
-                    axs[j, k].axis('off')
-                    count += 1
-            fig.savefig('output_%d.png' % i)
-    
-    # save final generator model
-    generator.save('final')
-
-gan_train(epoch=40000, batch_size=128, saving_interval=2000)
+          fig, axs = plt.subplots(5, 5)
+          count = 0
+          for j in range(5):
+              for k in range(5):
+                  axs[j, k].imshow(gen_images[count, :, :, 0], cmap='gray')
+                  axs[j, k].axis('off')
+                  count += 1
+          fig.savefig("output%d.png" % i)
+gan_train(40001, 32, 1000)
